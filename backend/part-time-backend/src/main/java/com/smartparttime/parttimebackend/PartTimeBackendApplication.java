bc209c58a708295f1964957c45468325bdac835a
Jayani Anuththara <jayanianuththara10@gmail.com>
2025-11-18 23:56:57 +0530

    Initial commit to backend

---
+++ backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/PartTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartparttime.parttimebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartTimeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartTimeBackendApplication.class, args);
+    }
+
+}
