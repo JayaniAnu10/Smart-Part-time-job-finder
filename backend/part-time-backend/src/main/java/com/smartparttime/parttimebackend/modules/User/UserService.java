a33eeb7fc41ef6ca01aa20f883bc621bd0c7abda
Jayani Anuththara <jayanianuththara10@gmail.com>
2025-11-30 19:40:29 +0530

    Add hashing password utility using BCryptPasswordEncoder

--- backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/User/UserService.java
+++ backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/User/UserService.java
@@ -4,6 +4,7 @@ import com.smartparttime.parttimebackend.modules.User.UserDtos.UserRegisterReque
 import com.smartparttime.parttimebackend.modules.User.UserDtos.UserRegisterResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
@@ -13,10 +14,12 @@ public class UserService{
     private final LanguageRepository languageRepository;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public  UserRegisterResponse registerUser(@Valid UserRegisterRequest request){
         Language language = languageRepository.findByLanguageIgnoreCase(request.getLanguage()).orElseThrow();
         var user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setLanguage(language);
         user.setCreatedAt(LocalDateTime.now());
         user.setIsVerified(false);
