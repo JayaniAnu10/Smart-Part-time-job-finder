1fe239e94cf91d0ae81ce1808d3d968390622611
Lakshika <lakshika111110@gmail.com>
2025-12-06 22:15:52 +0530

    Modified InputField component

---
+++ frontend/part-time-react/src/components/InputField.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface InputFieldProps {
+    label: string;
+    type?: string;
+    placeholder?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+    label,
+    type = "text",
+    placeholder,
+    value,
+    onChange,
+}) => {
+    return (
+        <div className="flex flex-col gap-1">
+            <label className="font-roboto text-[14px] font-medium text-[#0F1F3D]">{label}</label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                className="
+                    w-full px-4 h-[40px] 
+                    border border-[#A5A8AD] rounded-lg
+                    text-[14px] font-roboto text-[#111827]
+                    placeholder:text-[14px] placeholder:font-roboto placeholder:text-[#364D7D]
+                    focus:ring-1 focus:ring-[#FACC15]
+                    focus:border-[#FACC15] focus:outline-none
+                    transition-all duration-200"
+            />
+        </div>
+    );
+};
+
+export default InputField;
