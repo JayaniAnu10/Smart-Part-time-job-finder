304acab0ec866159360232ae68ae40ba9fdb1b85
Jayani Anuththara <jayanianuththara10@gmail.com>
2025-12-18 00:22:26 +0530

    Completed the qr code attendance management

--- backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/Attendance/AttendanceService.java
+++ backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/Attendance/AttendanceService.java
@@ -5,24 +5,70 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.smartparttime.parttimebackend.common.exceptions.BadRequestException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class AttendanceService {
 
+    private final AttendanceRepository attendanceRepository;
 
     public byte[] generateQr(String qrToken) throws WriterException, IOException {
 
-        String url = "http://localhost:8080/attendance/scan/token="+ qrToken;
+        String url = "http://localhost:8080/attendance/scan?token="+ qrToken;
 
         BitMatrix matrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, 300, 300);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         MatrixToImageWriter.writeToStream(matrix, "PNG", out);
         return out.toByteArray();
+    }
+
+    public ResponseEntity<String> scanQR(String token) {
+        var attendance = attendanceRepository.findByQrCode(token);
+
+        if (attendance == null) {
+            throw new BadRequestException("Invalid QR code: Attendance record not found.");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startTime = attendance.getSchedule().getStartDatetime();
+        LocalDateTime endTime = attendance.getSchedule().getEndDatetime();
+
+
+        if(attendance.getStatus() == AttendanceStatus.PENDING){
+            if (now.isAfter(endTime)) {
+                throw new BadRequestException("Cannot check in. The shift ended at " + endTime);
+            }
+
+            if(now.isBefore(startTime.minusHours(1))){
+                throw new BadRequestException("Too early .Cannot check in. The shift start at " + startTime);
+            }
+
+            attendance.setCheckInTime(LocalDateTime.now());
+            attendance.setStatus(AttendanceStatus.CHECKED_IN);
+            attendanceRepository.save(attendance);
+            return ResponseEntity.ok("Check in successfully");
+        }
+
+        if (attendance.getStatus() == AttendanceStatus.CHECKED_IN) {
+            if(now.isAfter(endTime.plusHours(8))) {
+                throw new BadRequestException("Check-out failed: Too much time has passed since shift end.");
+            }
+            attendance.setCheckOutTime(LocalDateTime.now());
+            attendance.setStatus(AttendanceStatus.CHECKED_OUT);
+            attendanceRepository.save(attendance);
+            return ResponseEntity.ok("Check-out successful");
+        }
+
+
+
+        return ResponseEntity.badRequest().body("Attendance already completed");
     }
 }
--- backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/Attendance/controller/AttendanceController.java
+++ backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/Attendance/controller/AttendanceController.java
@@ -1,45 +1,38 @@
 package com.smartparttime.parttimebackend.modules.Attendance.controller;
 
+import com.google.zxing.WriterException;
 import com.smartparttime.parttimebackend.common.exceptions.BadRequestException;
+import com.smartparttime.parttimebackend.modules.Application.repo.JobApplicationRepository;
 import com.smartparttime.parttimebackend.modules.Attendance.AttendanceRepository;
+import com.smartparttime.parttimebackend.modules.Attendance.AttendanceService;
 import com.smartparttime.parttimebackend.modules.Attendance.AttendanceStatus;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/attendance")
 public class AttendanceController {
     private final AttendanceRepository attendanceRepository;
+    private final AttendanceService attendanceService;
+    private final JobApplicationRepository jobApplicationRepository;
 
+    @PostMapping("/{id}")
+    public byte[] generateQr(@PathVariable UUID id) throws IOException, WriterException {
+        var attendance = attendanceRepository.findById(id).orElse(null);
+        if(attendance==null){
+            throw new BadRequestException("Attendance not found");
+        }
+        return attendanceService.generateQr(attendance.getQrCode());
+    }
 
     @GetMapping("/scan")
     public ResponseEntity<String> scanQr(@RequestParam String token){
-        var attendance = attendanceRepository.findByQrCode(token);
-
-        if(attendance.getSchedule().getEndDatetime().isBefore(LocalDateTime.now().plusDays(1))){
-            throw new BadRequestException("QR expired");
-        }
-
-        if(attendance.getSchedule().getStartDatetime().isAfter(LocalDateTime.now())){
-            throw new BadRequestException("Attendance is too early");
-        }
-
-        if(attendance.getStatus() == AttendanceStatus.PENDING){
-            attendance.setCheckInTime(LocalDateTime.now());
-            attendance.setStatus(AttendanceStatus.CHECKED_IN);
-            return ResponseEntity.ok("Check in successfully");
-        }
-
-        if (attendance.getStatus() == AttendanceStatus.CHECKED_IN) {
-            attendance.setCheckOutTime(LocalDateTime.now());
-            attendance.setStatus(AttendanceStatus.CHECKED_OUT);
-            return ResponseEntity.ok("Check-out successful");
-        }
-
-        return ResponseEntity.badRequest().body("Attendance already completed");
+        return attendanceService.scanQR(token);
     }
 }
