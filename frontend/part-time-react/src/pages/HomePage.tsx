0b157e62033281236ea3ac23206c487bc0893ebf
Lakshika <lakshika111110@gmail.com>
2025-12-21 10:29:04 +0530

    feat: add footer section and enhance home page layout

--- frontend/part-time-react/src/pages/HomePage.tsx
+++ frontend/part-time-react/src/pages/HomePage.tsx
@@ -2,8 +2,10 @@ import NavBar from "../components/NavBar";
 import HeroSection from "@/components/home/HeroSection";
 import WhyChooseSection from "@/components/home/WhyChooseSection";
 import BrowseByCategorySection from "@/components/home/BrowseByCategorySection";
-
-
+import RatingsSection from "@/components/home/RatingsSection";
+import HowItWorksSection from "@/components/home/HowItWorksSection";
+import CTASection from "@/components/home/CTASection";
+import FooterSection from "@/components/FooterSection";
 
 const HomePage = () => {
   return (
@@ -13,6 +15,10 @@ const HomePage = () => {
       <HeroSection />
       <WhyChooseSection />
       <BrowseByCategorySection />
+      <RatingsSection />
+      <HowItWorksSection/>
+      <CTASection />
+      <FooterSection/>
         
     </div>
   );
