5347e9eec44fbc63b2bc58510d021d5280b36540
Lakshika <lakshika111110@gmail.com>
2025-12-25 11:40:28 +0530

    update StatsSection

--- frontend/part-time-react/src/components/adminDashboard/StatsSection.tsx
+++ frontend/part-time-react/src/components/adminDashboard/StatsSection.tsx
@@ -55,7 +55,7 @@ export default function StatsSection() {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-15 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {stats.map((stat, index) => {
         const Icon = stat.icon;
         return (
