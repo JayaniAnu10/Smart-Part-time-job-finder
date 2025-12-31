020ef9bade66fe419a55a6811712e0b42b6a2cd8
Lakshika <lakshika111110@gmail.com>
2025-12-31 12:09:15 +0530

    feat: add view analytics page and charts

---
+++ frontend/part-time-react/src/pages/adminActions/ViewAnalytics.tsx
@@ -0,0 +1,9 @@
+// src/pages/adminActions/ViewAnalytics.tsx
+export default function ViewAnalytics() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">View Analytics</h1>
+      <p>Here you can see system analytics and statistics.</p>
+    </div>
+  );
+}
