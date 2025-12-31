8f63bcf642c20877df90b8100be1eb00780975ad
Lakshika <lakshika111110@gmail.com>
2026-01-01 00:30:35 +0530

    Create JobsByLocation.tsx component

---
+++ frontend/part-time-react/src/components/admin/analytics/JobsByLocation.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function JobsByLocation() {
+  return (
+    <Card className="rounded-2xl w-full h-[300px] max-w-full">
+      <CardHeader className="p-4 sm:p-6 md:p-8">
+        <CardTitle className="text-xl sm:text-left font-medium">Jobs by Location</CardTitle>
+      </CardHeader>
+
+      
+    </Card>
+  );
+}
