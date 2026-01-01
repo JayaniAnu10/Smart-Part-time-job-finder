b4c90835b7eeb12dff08f9ec1b534ccbdd38286a
Jayani Anuththara <jayanianuththara10@gmail.com>
2026-01-01 15:53:41 +0530

    Create the endpoint to get the user details according to the authenticated user.

--- backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/Auth/AuthController.java
+++ backend/part-time-backend/src/main/java/com/smartparttime/parttimebackend/modules/Auth/AuthController.java
@@ -3,15 +3,19 @@ package com.smartparttime.parttimebackend.modules.Auth;
 import com.smartparttime.parttimebackend.modules.Auth.Dtos.JwtResponse;
 import com.smartparttime.parttimebackend.modules.Auth.Dtos.UserLoginRequest;
 import com.smartparttime.parttimebackend.modules.Auth.Services.JwtService;
+import com.smartparttime.parttimebackend.modules.User.UserDtos.UserDto;
+import com.smartparttime.parttimebackend.modules.User.UserMapper;
+import com.smartparttime.parttimebackend.modules.User.UserService;
+import com.smartparttime.parttimebackend.modules.User.repo.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequestMapping("/auth")
 @RestController
@@ -19,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody UserLoginRequest request) {
@@ -30,5 +36,19 @@ public class AuthController {
         );
     var token = jwtService.generateToken(request.getEmail());
     return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+        var user = userRepository.findByEmail(email).orElse(null);
+
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 }
