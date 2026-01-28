dc9fb915b741cc34897e68938f351f85a27be7a7
Jayani Dahanayake <jayanianuththara10@gmail.com>
2026-01-28 10:57:04 +0530

    Revert "Lakshika"

--- frontend/part-time-react/src/pages/HomePage.tsx
+++ frontend/part-time-react/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-
+import NavBar from "../components/navBar/NavBar";
 import HeroSection from "@/components/home/HeroSection";
 import WhyChooseSection from "@/components/home/WhyChooseSection";
 import BrowseByCategorySection from "@/components/home/BrowseByCategorySection";
@@ -6,20 +6,15 @@ import RatingsSection from "@/components/home/RatingsSection";
 import HowItWorksSection from "@/components/home/HowItWorksSection";
 import CTASection from "@/components/home/CTASection";
 
-
 const HomePage = () => {
   return (
     <div className="relative pt-6">
-
-      
       <HeroSection />
       <WhyChooseSection />
       <BrowseByCategorySection />
       <RatingsSection />
       <HowItWorksSection />
       <CTASection />
-      
-        
     </div>
   );
 };
