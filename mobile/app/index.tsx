3d17c4e059722e656f5985b392a588bc0216e83e
Mudannayaka2001 <nimnadee2001@gmail.com>
2026-02-10 19:36:26 +0530

    changed

--- mobile/app/index.tsx
+++
@@ -1,9 +0,0 @@
-// app/index.tsx
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  //return <Redirect href="/WelcomeScreen" />;
-  //return <Redirect href="/SignupEmployer" />;
-  //return <Redirect href="/SelectRoleScreen" />;
-  return <Redirect href="/job/JobDetailsScreen" />;
-}
