cdc2f4b8a270f8f7582aeb7d2dc306cae56e1ca8
Lakshika <lakshika111110@gmail.com>
2026-03-09 21:18:03 +0530

    Add Selenium TestNG automation test for login page

---
+++ testing/selenium/ui-automation/src/main/java/base/BaseTest.java
@@ -0,0 +1,12 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    public BaseTest(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
